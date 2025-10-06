3c2977dbaba6d8d1117794b82cecf97a13776537
LaysC <laiscarvalho858@gmail.com>
2025-10-06 12:53:34 -0300

    Revert "Update Musica.java"
    This reverts commit 6f592a448253ff5d89be504b5a6db6af07a77f09.

--- src/main/java/org/acme/Musica.java
+++ src/main/java/org/acme/Musica.java
@@ -3,16 +3,16 @@ package org.acme;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.media.Schema; // Importação necessária
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "Musica") // <<< CORREÇÃO DEFINITIVA: Mapeia o nome da tabela
 public class Musica extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // CRÍTICO PARA O SWAGGER: Oculta o ID no Request Body (POST/PUT)
     @Schema(readOnly = true, example = "1")
     public Long id;
 
